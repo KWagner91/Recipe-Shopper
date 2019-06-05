b13e74b28ee13e723d17de96718d724f01f4df9d
kwagner91 <kerstin-wagner@gmx.at>
2019-06-05 14:55:09 +0200

    feat: Recipe details are displayed upon dummy click

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -8,8 +8,10 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Recipe Description', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d')
+    new Recipe('Test Recipe 1', 'Recipe Description', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'),
+    new Recipe('Test Recipe 2', 'Recipe Description', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d')
   ];
   
 
@@ -17,5 +19,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+ onRecipeSelected(recipe: Recipe) {
+  this.recipeWasSelected.emit(recipe);
+ }
 
 }
