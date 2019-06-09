9c925ddd0ce77fecf9260dd2dfb4c7f93a296bf6
kwagner91 <kerstin-wagner@gmx.at>
2019-06-09 22:52:29 +0200

    feat: Recipe Details show Ingredients to buy

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,14 +1,28 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>() ;
 
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe 1', 'Recipe Description', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'),
-        new Recipe('Test Recipe 2', 'Recipe Description', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d')
-      ];
+        new Recipe('Wiener Schnitzel', 
+        'An awsome Austrian Schnitzel', 
+        'https://live.staticflickr.com/7325/9299930183_72ce8e5424_b.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('Fries', 50)
+        ]),
+        new Recipe('Kaiserschmarrn', 
+        'A Tasty Austrian Sweet fresh out of the oven', 
+        'https://upload.wikimedia.org/wikipedia/commons/0/0a/Kaiserschmarrn_gro%C3%9F.jpg',
+        [
+            new Ingredient('Eggs', 4),
+            new Ingredient('Butter', 1),
+            new Ingredient('Sugar', 2)
+        ])
+    ];
 
     getRecipes() {
         return this.recipes.slice();
