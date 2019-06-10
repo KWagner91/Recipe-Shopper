b7c7b296f83252c0c82a3c25f7eb150b2cc403cc
kwagner91 <kerstin-wagner@gmx.at>
2019-06-10 09:55:34 +0200

    feat: user can send recipe items to shopping list

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 
 export class RecipeService {
 
@@ -24,8 +27,16 @@ export class RecipeService {
         ])
     ];
 
+    constructor(private slService: ShoppingListService) {
+
+    }
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 
 
