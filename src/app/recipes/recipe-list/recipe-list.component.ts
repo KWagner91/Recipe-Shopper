4a93767c2b5030e41885c00fc1e4eec6db1eb8d9
kwagner91 <kerstin-wagner@gmx.at>
2019-06-24 20:32:58 +0200

    feat: README was updated with instructions

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipes.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
