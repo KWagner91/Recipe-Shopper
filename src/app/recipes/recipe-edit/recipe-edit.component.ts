82764359456e335e0bbef0ebca0acf7889abfb96
kwagner91 <kerstin-wagner@gmx.at>
2019-06-24 19:55:22 +0200

    feat: redirect user after cancel and delete was clicked

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipes.service';
-import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -14,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -27,14 +26,19 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeForm.value);
     }
     else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
 
   onAddIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(
